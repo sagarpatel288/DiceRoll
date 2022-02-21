4132527d3fa71f37a6e7bc17e12a13f1d5298371
sagarpatelwithomniwyse <sagardhirajlal.patel@omniwyse.com>
2022-02-21 16:29:38 +0530

    Add comments

--- app/src/main/java/com/sr/dicepractice/screens/roll/ViewModelRoll.kt
+++ app/src/main/java/com/sr/dicepractice/screens/roll/ViewModelRoll.kt
@@ -9,9 +9,26 @@ class ViewModelRoll : ViewModel() {
 
     private val _rollValue = MutableLiveData(0)
 
-    val rollValueInt: LiveData<Int>
+    private val rollValueInt: LiveData<Int>
         get() = _rollValue
 
+    /**
+     * 1/24/2022 05:40
+     * <p>
+     * [rollValueString] is a normal [String]. However, its value
+     * keeps changing in sync with [rollValueInt] which is a [LiveData]
+     * and [rollValueInt] gets update via [_rollValue] which is a
+     * [MutableLiveData].
+     * </p>
+     * [_rollValue] is being updated at [onClickRoll] and [onClickReset].
+     * Update in [_rollValue] will update [rollValueInt] which is a [LiveData].
+     * </p>
+     * An update in [rollValueInt] will notify all the observers about the update.
+     * </p>
+     *
+     * @author srdpatel
+     * @since v0.0.1
+     */
     val rollValueString = Transformations.map(rollValueInt) {
         it?.let {
             if (it > 0) it.toString() else "Welcome!"
